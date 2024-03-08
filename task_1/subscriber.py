ab5e11f423bec5f081ce1ed8ae639a0ce90f4924
JonasSto <jonas.stoeckermann@gmx.de>
2024-03-08 11:37:03 +0100

    dawd

--- task_1/subscriber.py
+++ task_1/subscriber.py
@@ -1,1 +1,21 @@
-print("hi")
+import os
+import psycopg2
+
+# Lese das Passwort aus der Umgebungsvariable
+db_password = os.environ.get('PG_PASSWORD')
+
+print(db_password)
+
+# Verbinde dich mit der Datenbank
+try:
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password=db_password,
+        host="localhost",
+        port="5432"
+    )
+    print("Verbindung zur Datenbank hergestellt!")
+    # Führe hier deine Datenbankoperationen durch
+except psycopg2.OperationalError as e:
+    print(f"Fehler beim Verbinden mit der Datenbank: {e}")
