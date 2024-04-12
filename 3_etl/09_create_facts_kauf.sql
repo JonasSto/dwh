4019ea46fc2d7ddea4c0abc97e29f5d95bc99c80
mo-kap <morten.kapusta@gmx.de>
2024-04-12 11:38:49 +0200

    slide 9: add insert

--- 3_etl/09_create_facts_kauf.sql
+++ 3_etl/09_create_facts_kauf.sql
@@ -1,3 +1,4 @@
+INSERT INTO mart.fact_kauf (dim_fahrzeug_id, dim_kunde_id, dim_kfz_id, kauf_kalender_datum, liefer_kalender_datum, kaufpreis, rabatt_pct)
 SELECT fzg.dim_fahrzeug_id, kunde.dim_kunde_id, kfz.dim_kfz_id, k.kaufdatum, k.lieferdatum, k.kaufpreis, k.rabatt_pct
 FROM staging.fzg_kauf AS k
 JOIN mart.dim_fahrzeug AS fzg ON k.fin = fzg.fin
