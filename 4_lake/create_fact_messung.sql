d50c39abe6041a22ae4f6d67fc9c8c44e5a2d52b
mo-kap <morten.kapusta@gmx.de>
2024-04-12 14:25:01 +0200

    partitioning

--- 4_lake/create_fact_messung.sql
+++ 4_lake/create_fact_messung.sql
@@ -1,11 +1,15 @@
+DROP VIEW IF EXISTS mart.agg_sampling_1min;
+
 DROP TABLE IF EXISTS mart.fact_messung;
 CREATE TABLE mart.fact_messung (
     dim_fahrzeug_id INT,
     gesendet TIMESTAMP,
     empfangen TIMESTAMP,
     geschwindigkeit INT
-);
+) PARTITION BY RANGE (EXTRACT(YEAR FROM gesendet));
 
+CREATE TABLE mart.fact_messung_2023 PARTITION OF mart.fact_messung FOR VALUES FROM (2023) TO (2024); 
+CREATE TABLE mart.fact_messung_2024 PARTITION OF mart.fact_messung FOR VALUES FROM (2024) TO (2025);
 -- SELECT payload->>'fin' AS fin, payload->>'zeit' AS zeit, payload->>'geschwindigkeit' AS geschwindigkeit, payload->>'ort' AS ort
 -- FROM staging.messung;
 
@@ -13,5 +17,17 @@ INSERT INTO mart.fact_messung (dim_fahrzeug_id, gesendet, empfangen, geschwindig
 SELECT f.dim_fahrzeug_id, TO_TIMESTAMP((m.payload->>'zeit')::INT) AS gesendet, m.empfangen AS empfangen, (m.payload->>'geschwindigkeit')::INT AS geschwindigkeit
 FROM mart.dim_fahrzeug AS f
 JOIN staging.messung AS m ON f.fin = (m.payload->>'fin');
-SELECT *
-FROM mart.fact_messung;
+
+CREATE VIEW mart.agg_sampling_1min AS
+SELECT
+  dim_fahrzeug_id,
+  DATE_TRUNC('minute', gesendet) AS minute_interval,
+  ROUND(AVG(geschwindigkeit), 2) AS durch_geschw,
+  COUNT(*) AS anzahl
+FROM
+  mart.fact_messung
+GROUP BY
+  dim_fahrzeug_id,
+  minute_interval;
+
+SELECT * FROM mart.agg_sampling_1min;
